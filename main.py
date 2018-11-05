115fefa4d55db8d924e412ccd59f1b4fa149333a
kabardey <demirtasm16@itu.edu.tr>
2018-11-05 14:40:00 +0300

    average filter functions completed

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ class App(QMainWindow):
         imagePath, _ = QFileDialog.getOpenFileName()
         self.inputImg = cv2.imread(imagePath)
 
+
         pixmap_label = self.qlabel1
 
         height, width, channel = self.inputImg.shape
@@ -50,6 +51,125 @@ class App(QMainWindow):
     def saveImage(self):
         # This function is called when the user clicks File->Target Image.
         return NotImplementedError
+
+    def padding(self, padding_size, img):  # fill the outer border pixels according to kernel size
+
+        height, width, channel = img.shape
+
+        new_img = np.zeros((height + padding_size*2, width + padding_size*2, 3), dtype=np.uint8)
+        new_img[padding_size:(height+padding_size), padding_size:(width+padding_size), :] = img
+
+        return new_img
+
+    def avg_filter(self, kernel_size): # create average kernel
+
+        kernel = (1 / (kernel_size*kernel_size))*np.ones((kernel_size, kernel_size), np.float32)
+        return kernel
+
+    def conv(self, kernel, img, height, width, padding):
+
+        height2, width2, channel = self.inputImg.shape
+        temp_image = np.zeros((height2, width2, 3), dtype=np.uint8)
+
+        for i in range(padding, height+padding):
+            for j in range(padding, width+padding):
+
+                roi = img[i - padding:i + padding + 1, j - padding:j + padding + 1, 0] # take the part of image for convolution operation
+                conv_value = (roi * kernel).sum()
+                temp_image[i - padding, j - padding, 0] = int(conv_value)
+
+                roi = img[i - padding:i + padding + 1, j - padding:j + padding + 1, 1]  # take the part of image for convolution operation
+                conv_value = (roi * kernel).sum()
+                temp_image[i - padding, j - padding, 1] = int(conv_value)
+
+                roi = img[i - padding:i + padding + 1, j - padding:j + padding + 1, 2]  # take the part of image for convolution operation
+                conv_value = (roi * kernel).sum()
+                temp_image[i - padding, j - padding, 2] = int(conv_value)
+
+        # place the image to the qlabel
+        pixmap_label = self.qlabel1
+        bytesPerLine = 3 * width2
+        qImg = QImage(temp_image.data, width2, height2, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+        pixmap = QPixmap(qImg)
+        pixmap_label.setPixmap(pixmap)
+
+
+    def average_3(self):  # average filter 3x3
+        if (self.count != 1):
+            return QMessageBox.question(self, 'Error Message', "Please, load the image", QMessageBox.Ok, QMessageBox.Ok)
+
+        height, width, channel = self.inputImg.shape
+
+        image = self.padding(1, self.inputImg)  # add padding to the image
+        avg_kernel = self.avg_filter(3)  # create average kernel
+        self.conv(avg_kernel, image, height, width, padding=1)  # apply convolution
+
+
+    def average_5(self):  # average filter 5x5
+        if (self.count != 1):
+            return QMessageBox.question(self, 'Error Message', "Please, load the image", QMessageBox.Ok, QMessageBox.Ok)
+
+        height, width, channel = self.inputImg.shape
+
+        image = self.padding(2, self.inputImg)  # add padding to the image
+        avg_kernel = self.avg_filter(5)  # create average kernel
+        self.conv(avg_kernel, image, height, width, padding=2)  # apply convolution
+
+
+    def average_7(self):  # average filter 7x7
+        if (self.count != 1):
+            return QMessageBox.question(self, 'Error Message', "Please, load the image", QMessageBox.Ok, QMessageBox.Ok)
+
+        height, width, channel = self.inputImg.shape
+
+        image = self.padding(3, self.inputImg)  # add padding to the image
+        avg_kernel = self.avg_filter(7)  # create average kernel
+        self.conv(avg_kernel, image, height, width, padding=3)  # apply convolution
+
+
+    def average_9(self):  # average filter 9x9
+        if (self.count != 1):
+            return QMessageBox.question(self, 'Error Message', "Please, load the image", QMessageBox.Ok, QMessageBox.Ok)
+
+        height, width, channel = self.inputImg.shape
+
+        image = self.padding(4, self.inputImg)  # add padding to the image
+        avg_kernel = self.avg_filter(9)  # create average kernel
+        self.conv(avg_kernel, image, height, width, padding=4)  # apply convolution
+
+
+    def average_11(self):  # average filter 11x11
+        if (self.count != 1):
+            return QMessageBox.question(self, 'Error Message', "Please, load the image", QMessageBox.Ok, QMessageBox.Ok)
+
+        height, width, channel = self.inputImg.shape
+
+        image = self.padding(5, self.inputImg)  # add padding to the image
+        avg_kernel = self.avg_filter(11)  # create average kernel
+        self.conv(avg_kernel, image, height, width, padding=5)  # apply convolution
+
+
+    def average_13(self):  # average filter 13x13
+        if (self.count != 1):
+            return QMessageBox.question(self, 'Error Message', "Please, load the image", QMessageBox.Ok, QMessageBox.Ok)
+
+        height, width, channel = self.inputImg.shape
+
+        image = self.padding(6, self.inputImg)  # add padding to the image
+        avg_kernel = self.avg_filter(13)  # create average kernel
+        self.conv(avg_kernel, image, height, width, padding=6)  # apply convolution
+
+
+    def average_15(self):  # average filter 15x15
+        if (self.count != 1):
+            return QMessageBox.question(self, 'Error Message', "Please, load the image", QMessageBox.Ok, QMessageBox.Ok)
+
+        height, width, channel = self.inputImg.shape
+
+        image = self.padding(7, self.inputImg)  # add padding to the image
+        avg_kernel = self.avg_filter(15)  # create average kernel
+        self.conv(avg_kernel, image, height, width, padding=7)  # apply convolution
+
 
     def rotate_left(self):
         if (self.count != 1):
@@ -117,7 +237,6 @@ class App(QMainWindow):
         qImg = QImage(result_image.data, (width*scale), (height*scale), bytesPerLine, QImage.Format_RGB888).rgbSwapped()
         pixmap = QPixmap(qImg)
 
-        #pixmap = pixmap.scaled(300, 300, Qt.KeepAspectRatio)
         pixmap_label.setPixmap(pixmap)
 
     def scale_oneovertwox(self):
@@ -177,45 +296,67 @@ class App(QMainWindow):
         fileMenu.addAction(exitAct)
 
         average_filters = QMenu('Average Filters', self)
-        three = QAction('3x3', self)
-        five = QAction('5x5', self)
-        seven = QAction('7x7', self)
-        nine = QAction('9x9', self)
-        eleven = QAction('11x11', self)
-        thirteen = QAction('13x13', self)
-        fifteen = QAction('15x15', self)
+        three_avg = QAction('3x3', self)
+        five_avg = QAction('5x5', self)
+        seven_avg = QAction('7x7', self)
+        nine_avg = QAction('9x9', self)
+        eleven_avg = QAction('11x11', self)
+        thirteen_avg = QAction('13x13', self)
+        fifteen_avg = QAction('15x15', self)
 
-        average_filters.addAction(three)
-        average_filters.addAction(five)
-        average_filters.addAction(seven)
-        average_filters.addAction(nine)
-        average_filters.addAction(eleven)
-        average_filters.addAction(thirteen)
-        average_filters.addAction(fifteen)
+        three_avg.triggered.connect(self.average_3)
+        five_avg.triggered.connect(self.average_5)
+        seven_avg.triggered.connect(self.average_7)
+        nine_avg.triggered.connect(self.average_9)
+        eleven_avg.triggered.connect(self.average_11)
+        thirteen_avg.triggered.connect(self.average_13)
+        fifteen_avg.triggered.connect(self.average_15)
+
+        average_filters.addAction(three_avg)
+        average_filters.addAction(five_avg)
+        average_filters.addAction(seven_avg)
+        average_filters.addAction(nine_avg)
+        average_filters.addAction(eleven_avg)
+        average_filters.addAction(thirteen_avg)
+        average_filters.addAction(fifteen_avg)
 
         filters.addMenu(average_filters)
 
         gaussian_filters = QMenu('Gaussian Filters', self)
+        three_gaus = QAction('3x3', self)
+        five_gaus = QAction('5x5', self)
+        seven_gaus = QAction('7x7', self)
+        nine_gaus = QAction('9x9', self)
+        eleven_gaus = QAction('11x11', self)
+        thirteen_gaus = QAction('13x13', self)
+        fifteen_gaus = QAction('15x15', self)
 
-        gaussian_filters.addAction(three)
-        gaussian_filters.addAction(five)
-        gaussian_filters.addAction(seven)
-        gaussian_filters.addAction(nine)
-        gaussian_filters.addAction(eleven)
-        gaussian_filters.addAction(thirteen)
-        gaussian_filters.addAction(fifteen)
+        gaussian_filters.addAction(three_gaus)
+        gaussian_filters.addAction(five_gaus)
+        gaussian_filters.addAction(seven_gaus)
+        gaussian_filters.addAction(nine_gaus)
+        gaussian_filters.addAction(eleven_gaus)
+        gaussian_filters.addAction(thirteen_gaus)
+        gaussian_filters.addAction(fifteen_gaus)
 
         filters.addMenu(gaussian_filters)
 
         median_filters = QMenu('Median Filters', self)
+        three_med = QAction('3x3', self)
+        five_med = QAction('5x5', self)
+        seven_med = QAction('7x7', self)
+        nine_med = QAction('9x9', self)
+        eleven_med = QAction('11x11', self)
+        thirteen_med = QAction('13x13', self)
+        fifteen_med = QAction('15x15', self)
 
-        median_filters.addAction(three)
-        median_filters.addAction(five)
-        median_filters.addAction(seven)
-        median_filters.addAction(nine)
-        median_filters.addAction(eleven)
-        median_filters.addAction(thirteen)
-        median_filters.addAction(fifteen)
+        median_filters.addAction(three_med)
+        median_filters.addAction(five_med)
+        median_filters.addAction(seven_med)
+        median_filters.addAction(nine_med)
+        median_filters.addAction(eleven_med)
+        median_filters.addAction(thirteen_med)
+        median_filters.addAction(fifteen_med)
 
         filters.addMenu(median_filters)
 
